ced2b19022848302574909e8b2664af2194d7480
dev-ashanaur <devashanaur009@gmail.com>
2022-07-12 15:33:13 +0600

    messages endPoint created

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ import UserRoute from './Routes/UserRoute.js';
 import PostRoute from './Routes/PostRoute.js';
 import UploadRoute from './Routes/UploadRoute.js';
 import ChatRoute from './Routes/ChatRoute.js';
+import MessageRoute from './Routes/MessageRoute.js';
 import path from 'path';
 const app = express();
 dotenv.config();
@@ -60,6 +61,7 @@ app.use('/user',UserRoute);
 app.use('/post',PostRoute);
 app.use('/upload',UploadRoute);
 app.use('/chat',ChatRoute);
+app.use('/message',MessageRoute);
 
 app.use((err,req,res,next)=>{
     const errorStatus = err.status || 500;
